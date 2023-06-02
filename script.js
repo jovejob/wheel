0fee421dc28631cb91c75291fe733d0dd1ef972d
Jove Trajkoski <jovetrajkoskidev@gmail.com>
2023-06-02 16:08:15 +0200

    Update the wheel ajax/prize.json etc

--- script.js
+++ script.js
@@ -1,41 +1,132 @@
 // Immediately invoked function expression
 // to not pollute the global scope
 (function() {
+  // Constants for wheel sections
+  const sections = [
+    "Try Again",
+    "Try Again",
+    "WIN",
+    "Try Again",
+    "WIN",
+    "Try Again",
+    "WIN",
+    "Try Again",
+    "Try Again",
+    "Try Again",
+    "WIN",
+    "Try Again"
+  ];
+
+  // Wheel elements
   const wheel = document.querySelector('.wheel');
   const startButton = document.querySelector('.button');
-  let deg = 0;
+  const pointer = document.querySelector('.marker');
 
-  startButton.addEventListener('click', () => {
-    // Disable button during spin
-    startButton.style.pointerEvents = 'none';
+  // Initialize spin count
+  let spinCount = 0;
 
-    // Calculate a new rotation between 5000 and 10 000
-    deg = Math.floor(5000 + Math.random() * 5000);
+  // Disable start button initially
+  startButton.disabled = true;
+
+  // Calculate a random spin result
+  function getSpinResult() {
+    const randomIndex = Math.floor(Math.random() * sections.length);
+    return sections[randomIndex];
+  }
+
+  // AJAX call function
+  function getPrize() {
+    // todo Replace the URL with the actual endpoint for prize retrieval
+    // const url = 'https://whee/api/prize';
+    const url = 'prize.json';
+
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        // Handle the prize data
+        console.log('Prize:', data);
+        // Display the prize to the user
+        const resultElement = document.querySelector('.result');
+        resultElement.textContent = `Congratulations! You won ${data.prize}!`;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        // Display error message to the user
+        const resultElement = document.querySelector('.result');
+        resultElement.textContent = 'An error occurred. Please try again later.';
+      });
+  }
+
+  // Start the spin animation
+  function startSpin() {
+    // Disable start button during spin
+    startButton.disabled = true;
+
+    // Calculate a new rotation between 5000 and 10000
+    const deg = Math.floor(5000 + Math.random() * 5000);
 
     // Set the transition on the wheel
     wheel.style.transition = 'all 10s ease-out';
+
     // Rotate the wheel
     wheel.style.transform = `rotate(${deg}deg)`;
+
     // Apply the blur
     wheel.classList.add('blur');
-  });
 
-  wheel.addEventListener('transitionend', () => {
+    // Remove any previous result message
+    const resultElement = document.querySelector('.result');
+    resultElement.textContent = '';
+
+    // Wait for 1 second and stop the spin
+    setTimeout(stopSpin, 1000);
+  }
+
+  // Stop the spin animation
+  function stopSpin() {
     // Remove blur
     wheel.classList.remove('blur');
 
-    // Enable button when spin is over
-    startButton.style.pointerEvents = 'auto';
+    // Disable pointer events during stop
+    pointer.style.pointerEvents = 'none';
 
     // Need to set transition to none as we want to rotate instantly
     wheel.style.transition = 'none';
 
-    // Calculate degree on a 360 degree basis to get the "natural" real rotation
-    // Important because we want to start the next spin from that one
-    // Use modulus to get the rest value from 360
-    const actualDeg = deg % 360;
-    
+    // Calculate the actual rotation based on spin count
+    const actualDeg = 360 - (spinCount * 30) % 360;
+
     // Set the real rotation instantly without animation
     wheel.style.transform = `rotate(${actualDeg}deg)`;
-  });
+
+    // Increase spin count
+    spinCount++;
+
+    // Enable pointer events after the wheel has stopped
+    pointer.style.pointerEvents = 'auto';
+
+    // Check the spin result
+    const spinResult = getSpinResult();
+    console.log('Spin Result:', spinResult);
+    const resultElement = document.querySelector('.result');
+    resultElement.textContent = spinResult;
+
+    if (spinResult === 'WIN') {
+      // Perform AJAX call to retrieve prize
+      getPrize();
+    } else {
+      // Check if the player has more spins
+      if (spinCount < 2) {
+        // Enable start button for another spin
+        startButton.disabled = false;
+      } else {
+        // Display "Try next time" message
+        const resultElement = document.querySelector('.result');
+        resultElement.textContent = 'Sorry, better luck next time!';
+      }
+    }
+  }
+
+  // Event listener for start button
+  startButton.addEventListener('click', startSpin);
 })();
